e38d0093f17490b0e2494d807fdd04d53946dbdc
KenwoodFox <joe@kitsunehosting.net>
2026-01-19 15:12:14 -0500

    Inital

---
+++ conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'EZ-Bake-Docs'
+copyright = '2026, Matthew, Joe'
+author = 'Matthew, Joe'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.todo']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'scrolls'
+html_static_path = ['static']
+html_logo = 'static/EZBakeLogo.png'
+html_css_files = ['custom.css']
---
+++ index.rst
@@ -0,0 +1,31 @@
+.. EZ-Bake-Docs documentation master file, created by
+   sphinx-quickstart on Mon Jan 19 14:42:27 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+EZ-Bake-Docs documentation
+==========================
+
+What is a Capstone Project?
+---------------------------
+
+Do a big job
+
+
+Who's on the team?
+------------------
+
+.. note::
+
+   Matthew is the best guy.
+
+   .. image:: static/matthew.png
+      :alt: Picture of Matthew
+      :align: center
+      :height: 200px
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
